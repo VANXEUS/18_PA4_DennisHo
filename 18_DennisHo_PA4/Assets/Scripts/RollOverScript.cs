275fd7e15e126214e3d22df016865d4da3183617
VANXEUS <diamonddialgabs@gmail.com>
2020-04-30 14:03:13 +0800

    Added Unity Project
    Added a unity project

---
+++ 18_DennisHo_PA4/Assets/Scripts/RollOverScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollOverScript : MonoBehaviour
+{
+    private Vector3 target = new Vector3(2.0f, 0.0f, 0.0f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(target, Vector3.up, 110 * Time.deltaTime);
+    }
+}
